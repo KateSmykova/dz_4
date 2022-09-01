ab646d347b5dabf74cd9a245dc26a562bd039668
Katya <katya0897@yandex.ru>
2022-09-01 22:54:34 +0300

    z_25_resh

---
+++ z_27/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите число N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+  int SumNum(int num)
+  {  
+    int count = Convert.ToString(num).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < count; i++)
+    {
+      advance = num - num % 10;
+      result = result + (num - advance);
+      num = num / 10;
+    }
+   return result;
+  }
+
+int sumNum = SumNum(num);
+Console.WriteLine("Сумма цифр в числе: " + sumNum);
