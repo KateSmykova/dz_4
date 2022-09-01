8bf7f97b3558ee126fedd71b9bb1fc624ec24190
Katya <katya0897@yandex.ru>
2022-09-01 22:55:36 +0300

    resh_dz4

---
+++ z_25/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число В: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+double d = Math.Pow(a,b);
+
+Console.WriteLine(d);
